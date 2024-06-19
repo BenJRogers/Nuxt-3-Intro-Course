c157039a3d126bee85c84425c10e3212968a7b63
Benjamin <benjamin.rogers@merkleinc.com.au>
2024-06-19 21:49:30 +1000

    started page layout

---
+++ components/Sidebar/index.vue
@@ -0,0 +1,7 @@
+<script setup>
+const loading = ref(false);
+</script>
+
+<template>
+    <p>This is the sidebar</p>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="flex items-start">
+        <Sidebar />
+        <div class="">
+            <slot />
+        </div>
+    </div>
+</template>
